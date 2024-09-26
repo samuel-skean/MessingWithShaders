5353d7bd105af7bf8fa91f68e5aab09b0186c3b9
Samuel Skean <sskean2@uic.edu>
2024-09-26 14:26:27 -0500

    Think fast!

--- p5/First/sketch.js
+++ p5/First/sketch.js
@@ -1,20 +1,23 @@
 let exampleShader;
+let backgroundImage;
 
 function preload() {
-  exampleShader = loadShader('example.vert', 'example.frag');
+  exampleShader = loadShader('example.vert.glsl', 'example.frag.glsl');
+  backgroundImage = loadImage('think_fast.jpg');
 }
 
 
 function setup() {
   createCanvas(400, 400, WEBGL);
   shader(exampleShader);
+  
   noStroke();
 }
 
 function draw() {
   clear();
-  
-  exampleShader.setUniform("millis", millis());
+  exampleShader.setUniform("background", backgroundImage);
+  exampleShader.setUniform("millis", millis() % 1000);
   //run shader
   rect(0, 0, width, height);
 }
